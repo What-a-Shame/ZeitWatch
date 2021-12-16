3fddb9aef26724ccec30fe38c0f94f507be6237e
Shame <07licenciaparamatar@gmail.com>
2021-12-16 12:55:36 +0100

    Boton del menu con animacion

--- app.js
+++ app.js
@@ -16,5 +16,10 @@ const menu_hamburguesa = (menu) => {
 menu.addEventListener("click",
     () => {
         menu_hamburguesa(header);
+        if (!menu.classList.contains("active")) {
+            menu.classList.add("active");
+        } else {
+            menu.classList.remove("active");
+        }
     }
 );
